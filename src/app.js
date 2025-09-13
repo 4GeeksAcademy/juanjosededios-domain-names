c18b6f8091e3f798f54c9eab330e8e0cde946dba
juanjooddm <juanjoseddm17052009@gmail.com>
2025-09-13 09:08:41 +0000

    domainjs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let element = document.querySelector(".domain")
+  const pronoun = ['the', 'our'];
+
+  const adj = ['great', 'big'];
+
+  const noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    //pronoun.forEach(pronoun => {     Lo comento porque se puede hacer tanto con el 1er forEach o con el for 
+    adj.forEach(adj => {
+      noun.forEach(nouns => {
+        const nuevoP = document.createElement("p")
+        nuevoP.innerHTML = pronoun[i] + adj + nouns + ".com"
+        element.appendChild(nuevoP)
+      });
+    });
+    //});
+  };
 };
